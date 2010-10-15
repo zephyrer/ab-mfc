ea8f7a506c707241c7d6e804595a3e66e83c6840
superkiki1989 <superkiki1989@ce34bfc1-e555-5f21-4a2b-332ae8037cd2>
2010-10-15 09:01:56 +0000

    增加了调试控制台

--- trunk/ab_mfc/ab_mfc/resource.h
+++ trunk/ab_mfc/ab_mfc/resource.h
@@ -52,13 +52,15 @@
 #define ID_ICON_32776                   32776
 #define ID_ICON_32777                   32777
 #define ID_1_1                          32778
+#define ID_32779                        32779
+#define ID_CONSOLE                      32780
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        138
-#define _APS_NEXT_COMMAND_VALUE         32779
+#define _APS_NEXT_COMMAND_VALUE         32781
 #define _APS_NEXT_CONTROL_VALUE         1032
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
